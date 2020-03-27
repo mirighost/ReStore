9e2227788e4476e9962dc48d34093dbf56714739
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-03-27 20:49:08 +0400

    added styles

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -4,12 +4,16 @@ export default class BookstoreService {
             {
                 id: 1,
                 title: 'Production-Ready Microservices',
-                author: 'Susan J. Fowler'
+                author: 'Susan J. Fowler',
+                price: 32,
+                coverImage: "https://images-na.ssl-images-amazon.com/images/I/51oxXEG6TRL._SX379_BO1,204,203,200_.jpg"
             },
             {
                 id: 2,
                 title: 'Release It!',
-                author: 'Michael T. Nygard'
+                author: 'Michael T. Nygard',
+                price: 45,
+                coverImage: "https://images-na.ssl-images-amazon.com/images/I/419zAwJJH4L._SX415_BO1,204,203,200_.jpg"
             }
         ];
     };
